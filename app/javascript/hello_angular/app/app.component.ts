66659222bc7708703a2f422b8b13f144bc7dbe8d
Pascal Bestel <pascal.bestel@outlook.com>
2017-10-30 15:45:28 +0200

    I have it running

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-angular',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `<h1>Hello {{name}} What else</h1>`
 })
 export class AppComponent {
   name = 'Angular!';
