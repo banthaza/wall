45cee6b151af54c5fc7f6c2f88884e4077915e57
Pascal Bestel <pascal.bestel@outlook.com>
2017-11-09 21:06:56 +0200

    Fixing the table

--- db/migrate/20171106144342_document_types_table.rb
+++
@@ -1,9 +0,0 @@
-class DocumentTypesTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :countries do |t|
-      t.string :iso_code
-      t.string :name
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20171107140839_add_countries_table.rb
+++
@@ -1,9 +0,0 @@
-class AddCountriesTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :countries do |t|
-      t.string :name
-      t.string :code
-      t.timestamps
-    end
-  end
-end
